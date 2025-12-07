ec27edefb1a9605a5eb117d8682c8950aea18100
VivianMak <vivianmak.30@gmail.com>
2025-12-07 16:38:36 -0500

    one iteration of icp working with visuals

--- src/pc_icp2.py
+++ src/pc_icp2.py
@@ -18,20 +18,87 @@ def find_nearest_neighbors(source_pc, target_pc, nearest_neigh_num):
 
 
 
+def main():
 
-# Files to look at
-pcd_files = [3,4]
-cloud_pts = {}
+    # Files to look at
+    pcd_files = [4,5]
+    cloud_pts = {}
 
-# Saving points (x,y,z) to dictionary
-for n in pcd_files:
-    file = f"data/bun0{n}.pcd"
-    print(f"Looking at: {file}")
+    # Saving points (x,y,z) to dictionary
+    for n in pcd_files:
+        file = f"data/bun0{n}.pcd"
 
-    # Add points to dictionary as cloud objects
-    cloud = PyntCloud.from_file(file)
-    cloud_pts[n] = cloud #.points
+        # Add points to dictionary as cloud objects
+        cloud = o3d.io.read_point_cloud(file)
+        cloud_pts[n] = cloud #.points
 
-find_nearest_neighbors(cloud_pts[3], cloud_pts[4], 2)
+        print(f"Looking at: {file} has {len(cloud.points)} points.")
 
+    source = cloud_pts[4]
+    target = cloud_pts[5]
+
+    print("---- PERFORMING ICP -------")
+    new = icp(source, target)
+
+    print("----VISUALIZING-----")
+    visualize(cloud_pts, new)
+
+
+def icp(source, target):
+    source.paint_uniform_color([0.5, 0.5, 0.5])
+    target.paint_uniform_color([0, 0, 1])
+
+    target_points = np.asarray(target.points)
+
+    # 1. Find nearest neighbor
+    new_source_points = find_nearest_neighbors(source, target, 1)
+
+    print(f"There are {len(new_source_points)} nearest neighbor points")
+    print(f"They are of type {new_source_points.dtype}")
+    print(new_source_points[4])
+
+    # 2. Find point cloud centroids and their repositions
+    target_points = np.asarray(target.points)
+    source_centroid = np.mean(new_source_points, axis=0)
+    target_centroid = np.mean(target_points, axis=0)
+    source_repos = np.zeros_like(new_source_points)
+    target_repos = np.zeros_like(target_points)
+    source_repos = np.asarray([new_source_points[ind] - source_centroid for ind in range(len(new_source_points))])
+    target_repos = np.asarray([target_points[ind] - target_centroid for ind in range(len(target_points))])
+
+    # 3. Find correspondence between source and target point clouds
+    cov_mat = target_repos.transpose() @ source_repos
+
+    print("The covariance matrix is:")
+    print(cov_mat)
+
+    U, X, Vt = np.linalg.svd(cov_mat)
+    R = U @ Vt
+    t = target_centroid - R @ source_centroid
+    t = np.reshape(t, (1,3))
+    curr_cost = np.linalg.norm(target_repos - (R @ source_repos.T).T)
+    print("Curr_cost=", curr_cost)
+
+    return new_source_points
+
+def visualize(cloud_pts, new_pts):
+
+    visuals = []
+    
+    for cloud in cloud_pts.values():
+        visuals.append(cloud)
+
+    # For new points, turn into a point cloud object
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(np.asarray(new_pts))
+
+    visuals.append(pcd)
+
+    o3d.visualization.draw_geometries(visuals,
+                                  window_name="PCD 4,5 with nearest neighbor new points",
+                                  width=800, height=600)
+
+
+if __name__ == "__main__":
+    main()
 
