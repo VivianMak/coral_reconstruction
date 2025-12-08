e5cc74eef420d3b2bf12f04cbf87b72bd9baf0c4
VivianMak <vivianmak.30@gmail.com>
2025-12-07 23:23:50 -0500

    check open3d visuals

--- src/pc_visual.py
+++ src/pc_visual.py
@@ -2,34 +2,56 @@ from pyntcloud import PyntCloud
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-# for i in range(1,6):
+for i in range(4,6):
 
-#     file = f"data/bun0{i}.pcd"
-#     print(f"Looking at: {file}")
+    file = f"data/bun0{i}.pcd"
+    print(f"Looking at: {file}")
 
-#     cloud = PyntCloud.from_file(file)
-#     points = cloud.points
+    cloud = PyntCloud.from_file(file)
+    points = cloud.points
 
-#     # print(points.head())
+    # print(points.head())
     
-#     ax.scatter(points.x, points.y, points.z, s=1)
+    ax.scatter(points.x, points.y, points.z, s=1)
 
-# ax.set_xlabel("X") 
-# ax.set_ylabel("Y") 
-# ax.set_zlabel("Z")
-# ax.set_title("Point Cloud Scatter Plot")
+ax.set_xlabel("X") 
+ax.set_ylabel("Y") 
+ax.set_zlabel("Z")
+ax.set_title("Point Clouds 4 and 5 Scatter Plot")
+ax.set_axis_off()
 
-# plt.show()
+plt.show()
 
-# print(points.head())
-# print(points.dtypes)        # x,y,z
+print(points.head())
+print(points.dtypes)        # x,y,z
 
 
 
-########## POINT CLOUD DATA #########
+###### USING OPEN3D #############
+
+# import open3d as o3d
+
+# for i in range(6):
+
+#     # Load the PCD file
+#     pcd = o3d.io.read_point_cloud(f"data/bun0{i}.pcd")
+
+#     # Check what was loaded
+#     print(pcd)
+#     print("Has normals:", pcd.has_normals())
+
+#     # Visualize
+#     o3d.visualization.draw_geometries(
+#         [pcd],
+#         point_show_normal=True,   # <- show the surface normals
+#         window_name="Stanford Bunny"
+#     )
+
+
+########## POINT CLOUD DATA INFO #########
 # PointCloud with 397 points.
 # Has normals: True
 # PointCloud with 200 points.
@@ -42,22 +64,3 @@ from mpl_toolkits.mplot3d import Axes3D
 # Has normals: False
 # PointCloud with 397 points.
 # Has normals: False
-
-
-import open3d as o3d
-
-for i in range(6):
-
-    # Load the PCD file
-    pcd = o3d.io.read_point_cloud(f"data/bun0{i}.pcd")
-
-    # Check what was loaded
-    print(pcd)
-    print("Has normals:", pcd.has_normals())
-
-    # Visualize
-    o3d.visualization.draw_geometries(
-        [pcd],
-        point_show_normal=True,   # <- show the surface normals
-        window_name="Stanford Bunny"
-    )
