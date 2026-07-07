f0205439fe9dce54f7c41675466ab48f9f24d11a
Vivian Mak <vivianmak.30@gmail.com>
2026-07-07 19:18:52 -0400

    working detecting affected for 1 pcd

--- WarpField.py
+++ WarpField.py
@@ -1,7 +1,8 @@
 
 import numpy as np
-
 import open3d as o3d
+
+RADIUS = 0.005
 
 class WarpField():
 
@@ -20,17 +21,11 @@ class WarpField():
             self.pcd_files.append(o3d.io.read_point_cloud(item))
 
 
-    def draw_sphere(self, selected_idx):
+    def draw_sphere(self, selected_points):
         """Draw transparent spheres to visualize deformation node."""
-        
-        # overwrite for now
-        p1 = np.array([-0.045, 0.006, 0.11])        #22018 (-0.045, 0.006, 0.11)
-        p2 = np.array([-0.0013, -0.046, 0.096])     #3565 (-0.0013, -0.046, 0.096)
-        selected_idx = [p1, p2]
-
 
         # Create a sphere for each point
-        sphere_radius = 0.005  # Adjust this depending on your point cloud scale
+        sphere_radius = RADIUS  # Adjust this depending on your point cloud scale
         sphere_color = [1.0, 0.0, 0.0, 0.4]  # Bright red, alpha < 1.0 = translucent
 
         # Material with a transparency-capable shader; alpha comes from base_color
@@ -38,7 +33,7 @@ class WarpField():
         sphere_material.shader = "defaultLitTransparency"
         sphere_material.base_color = sphere_color
 
-        for i, pt in enumerate(selected_idx):
+        for i, pt in enumerate(selected_points):
             # Create a primitive sphere centered at [0, 0, 0]
             sphere = o3d.geometry.TriangleMesh.create_sphere(radius=sphere_radius)
             sphere.compute_vertex_normals()
@@ -52,6 +47,22 @@ class WarpField():
                 "geometry": sphere,
                 "material": sphere_material,
             })
+
+    
+    def find_affected(self, sphere_centers):
+        """Within sphere centers (deformation nodes) return the affected points"""
+
+        for pcd in self.pcd_files:
+            pcd_tree = o3d.geometry.KDTreeFlann(pcd)
+            all_points = np.asarray(pcd.points)
+
+            for center in sphere_centers:
+                # Finds all indices inside the radius around this center
+                [k, idx, _] = pcd_tree.search_radius_vector_3d(center, RADIUS)
+                points_inside = all_points[idx]
+
+                print(points_inside)
+
 
 
 
@@ -69,7 +80,6 @@ class WarpField():
 
 
         if interactive:
-            selected_idxs = []
             vis = o3d.visualization.VisualizerWithEditing()
             vis.create_window()
             for pcd in self.pcd_files:
@@ -77,13 +87,19 @@ class WarpField():
             vis.run()  # The window will pop up. Press Shift+Left Click to select points.
             vis.destroy_window()
 
-            # Show indicies of picked
+            # Convert picked indices into actual xyz coordinates
             idx = vis.get_picked_points()
-            selected_idxs.append(np.array([idx]))
+            selected_points = np.asarray(self.pcd_files[0].points)[idx]
             print("Selected point indices:", idx)
 
-            if selected_idxs is not None:
-                self.draw_sphere(selected_idxs)
+            # overwrite for now
+            p1 = np.array([-0.045, 0.006, 0.11])        #22018 (-0.045, 0.006, 0.11)
+            p2 = np.array([-0.0013, -0.046, 0.096])     #3565 (-0.0013, -0.046, 0.096)
+            selected_points = [p1, p2]
+
+            if len(selected_points) > 0:
+                self.draw_sphere(selected_points)
+                self.find_affected(selected_points)
         
         for i, pcd in enumerate(self.pcd_files):
             self.geometries_to_visualize.append({"name": f"pcd_{i}", "geometry": pcd})
